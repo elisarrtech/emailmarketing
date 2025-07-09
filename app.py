894dceba5cd37c8f7b94b77a5ce022a40364d347
elisarrtech <146400511+elisarrtech@users.noreply.github.com>
2025-07-08 21:20:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from services.email_service import send_email
 import os
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
 app = Flask(__name__)
 app.secret_key = 'clave_secreta_para_flash'
 
